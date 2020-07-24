3ee0381ac0e164daa1cd0392a23bf36bc88ef429
Rohan Singh <1andonly1rohan@gmail.com>
2020-07-24 23:22:57 +0530

    Updates

---
+++ precache-manifest.c5679716e7dde55560a161d95e11a4fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69b568baa99f73f236dcb6a068ca088b",
+    "url": "/moster-rolex/index.html"
+  },
+  {
+    "revision": "2d5e4d7d62bbcee46c91",
+    "url": "/moster-rolex/static/css/main.7cf3f106.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/moster-rolex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moster-rolex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d5e4d7d62bbcee46c91",
+    "url": "/moster-rolex/static/js/main.85edbb5d.chunk.js"
+  },
+  {
+    "revision": "4ff9da14d198a119f193",
+    "url": "/moster-rolex/static/js/runtime-main.1cb5792c.js"
+  }
+]);
